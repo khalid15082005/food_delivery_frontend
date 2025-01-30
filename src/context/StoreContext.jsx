c8334ed37aa42406705dd4aa104c97a8177610d0
khalid15082005 <ksheik941@gmail.com>
2025-01-30 15:39:15 +0530

    change the url

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -7,7 +7,8 @@ const StoreContextProvider = (props) => {
 
 
     const[cartItems,setCartItems] =useState({});
-    const url ="http://localhost:4000"
+    // const url ="http://localhost:4000"
+    const url ="https://food-delivery-backend-dsc9.onrender.com"
     const [token,setToken] = useState("");
     const [food_list,setFoodList] = useState([]);
 
