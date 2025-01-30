f8f7a86e49c19f470e178348a357bc24efecc5ac
khalid15082005 <ksheik941@gmail.com>
2025-01-30 10:17:01 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>
+        A food delivery menu description is a description of the food items available for delivery from a restaurant, store, or independent food delivery company
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Header
